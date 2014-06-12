82172b800eaa5aadef8be09a67f37d59a7038533
James Zinger <dev@jameszinger.com>
2014-06-12 17:38:06 -0400

    Testing mailer with the "helocheck@cbl.abuseat.org" checker

--- server.js
+++ server.js
@@ -17,15 +17,55 @@ GLOBAL._ = underscore._;
 // Export pipeline object as a utility
 exports.pipeline = require('./lib/pipeline');
 
-// Parse the configuration file from the application
-var configStr   = fs.readFileSync('BridgeConfig.json','utf8');
-var config      = JSON.parse(JSON.minify(configStr));
+var config;
+
+// Check if the file exists. if not create a default configuration to use
+if ( !fs.existsSync( 'BridgeConfig.json' ) ) {
+    config = {
+        "default": true,
+        server: {
+            mode: "insecure",
+            port: 3000
+        },
+        database: {
+            user: "root",
+            password: "",
+            host: "localhost",
+            database: "bridge"
+        },
+        logger: {
+            exception: {
+                filename: "logs/exceptions.log",
+                writetoconsole: true
+            },
+            server: {
+                filename: "logs/server.log",
+                level: "debug",
+                consoleLevel: "warn"
+            },
+            console: {
+                level: "info"
+            }
+        },
+        mailer: {
+            method: "Direct",
+            options: {}
+        }
+    };
+}
+// If the file exists load it.
+else
+{
+    // Parse the configuration file from the application
+    var configStr = fs.readFileSync( 'BridgeConfig.json', 'utf8' );
+    config = JSON.parse( JSON.minify( configStr ) );
+}
 
 // Start the express app
 GLOBAL.app         = express();
 
-app.set('SecureMode', config.server.mode);
-app.set('BridgeConfig', config);
+app.set( 'SecureMode', config.server.mode );
+app.set( 'BridgeConfig', config );
 
 // Determine the port to listen on
 var port = config.server.port || 3000;
@@ -36,15 +76,15 @@ exports.app = app;
 exports.config = config;
 
 // Read in local modules
-var loggerObj  = require('./src/logger');
-var database   = require('./src/database');
-var filters    = require('./src/filters');
-var bridgeWare = require('./src/middleware');
-var pipelines  = require('./src/pipelines');
-var mailer     = require('./src/mailer');
+var loggerObj  = require( './src/logger' );
+var database   = require( './src/database' );
+var filters    = require( './src/filters' );
+var bridgeWare = require( './src/middleware' );
+var pipelines  = require( './src/pipelines' );
+var mailer     = require( './src/mailer' );
 
 // Prepare server variable
-var server     = null;
+var server = null;
 
 // Export local files for the API to use
 exports.filters         = filters;
@@ -52,68 +92,73 @@ exports.bridgePipelines = pipelines;
 
 // Prepare a steam in which for express to be able to write to winston
 var logStream = {
-    write: function(message, encoding) {
-        app.get('logger').verbose('Request: ' + message);
+    write: function ( message, encoding ) {
+        app.get( 'logger' ).verbose( 'Request: ' + message );
     }
 };
 
 // Setting standard dictionary objects
 // database reference
-app.set('database', database);
+app.set( 'database', database );
 
 // Tell express that it is behind a proxy
-app.enable('trust proxy');
+app.enable( 'trust proxy' );
 
 /////////////////////////////////////////////////////////////////////////////////////////
 ///////    STARTING SETUP OF MIDDLEWARE    //////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
 
 // Use the logging middleware to log requests using the stream above
-app.use(express.logger({stream: logStream}));
+app.use( express.logger( {
+    stream: logStream
+} ) );
 
 // Server any static content under that client folder
 // Should be first due to wanting to server content before API whatever happens.
-app.use(express.static('client'));
+app.use( express.static( 'client' ) );
 
 // development only settings
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+if ( 'development' == app.get( 'env' ) ) {
+    app.use( express.errorHandler( {
+        dumpExceptions: true,
+        showStack: true
+    } ) );
 }
 
 // production only settings
-if ('production' == app.get('env')) {
-    app.use(express.errorHandler());
+if ( 'production' == app.get( 'env' ) ) {
+    app.use( express.errorHandler() );
 }
 
 // Automatically parse the body to JSON
-app.use(express.json());
+app.use( express.json() );
 
 // Decode URL Strings
-app.use(express.urlencoded());
+app.use( express.urlencoded() );
 
 // Provides faux HTTP method support.
 //app.use(express.methodOverride());
 
 // Add the CORS headers to the response
-app.use(bridgeWare.attachCORSHeaders);
+app.use( bridgeWare.attachCORSHeaders );
 
 // Handle CORS Request
-app.use(bridgeWare.handleOptionsRequest);
+app.use( bridgeWare.handleOptionsRequest );
 
 // create the Bridge Objects on the request and response
-app.use(bridgeWare.prepareBridgeObjects);
+app.use( bridgeWare.prepareBridgeObjects );
 
 // read the query string from a request and parse it as JSON
-app.use(bridgeWare.parseGetQueryString);
+app.use( bridgeWare.parseGetQueryString );
 
 // Standard Request Middleware for Verification of content for any API Calls
-app.use(bridgeWare.verifyRequestStructure);
+app.use( bridgeWare.verifyRequestStructure );
 
 // Use the router to route messages to the appropriate locations
-app.use(app.router);
+app.use( app.router );
 
 // Prepare the bridge response headers on the response object
-app.use(bridgeWare.setupResponseHeaders);
+app.use( bridgeWare.setupResponseHeaders );
 
 /////////////////////////////////////////////////////////////////////////////////////////
 ///////     MIDDLEWARE SETUP COMPLETE      //////////////////////////////////////////////
@@ -200,21 +245,19 @@ setTimeout( function () {
     }
 
     var mail = {
-        to: "Joey.Zinger.99@gmail.com",
-        from: "info@jameszinger.com",
-        subject: "Testing Bridge Mailer",
-        text: "This is the message i am sending you. This is a for reals test right now.",
+        to: "helocheck@cbl.abuseat.org",
+        from: "dev@jameszinger.com",
+        subject: "Testing Mailer",
         html: "<h1>This is the HTML Body</h1>"
     };
 
-    app.get('logger').info('Attempting to send mail', mail);
-    if (mailer.sendMail(mail)) 
-    {
-        app.get('logger').info('Mail sent successfully');
-    }
-    else
-    {
-        app.get('logger').info("Mail didn't send successfully", mail);
+    app.get( 'logger' ).info( 'Attempting to send mail', mail );
+
+    if ( mailer.sendMail( mail ) ) {
+        app.get( 'logger' ).info( 'Mail sent successfully' );
+    } 
+    else {
+        app.get( 'logger' ).info( "Mail didn't send successfully", mail );
     }
 
 }, 1000 );
