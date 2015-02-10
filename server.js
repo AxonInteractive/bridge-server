51db42dc1759d5a2fe593cbebbe02567bdcf88bd
James Zinger <Joey.Zinger.99@gmail.com>
2015-02-10 15:49:51 -0500

    Hopefully disabled SSLv3

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ var bodyParser = require( 'body-parser' );
 var onHeaders  = require( 'on-headers'  );
 var userAgent  = require( 'express-useragent' );
 var moment     = require( 'moment' );
-var compress = require('compression');
+var compress   = require('compression');
+var constants  = require('constants');
 
 Q.longStackSupport = true;
 
@@ -277,7 +278,21 @@ if ( config.server.mode === "https" ) {
         var credentials = {
             key: results.shift(),
             cert: results.shift(),
-            ca: results
+            ca: results,
+            //
+            // This is the default secureProtocol used by Node.js, but it might be
+            // sane to specify this by default as it's required if you want to
+            // remove supported protocols from the list. This protocol supports:
+            //
+            // - SSLv2, SSLv3, TLSv1, TLSv1.1 and TLSv1.2
+            //
+            secureProtocol: 'SSLv23_method',
+
+            //
+            // Supply `SSL_OP_NO_SSLv3` constant as secureOption to disable SSLv3
+            // from the list of supported protocols that SSLv23_method supports.
+            //
+            secureOptions: constants.SSL_OP_NO_SSLv3,
         };
 
         server = https.createServer( credentials, app );
